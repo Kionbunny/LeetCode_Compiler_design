61f8ca588b60486c141577075ee9b42bcd1e08f5
masha <bunnymummy2002@gmail.com>
2025-03-16 21:31:20 +0530

    one comment is added

--- src/ICompiler.java
+++ src/ICompiler.java
@@ -1,3 +1,4 @@
+// make interface ICompiler as public to use it in other child class as well as the Client class(Main class)
 public interface ICompiler {
     // interfaces will have only the method declarations implementations are handled by its child class
      void compile(String code);
